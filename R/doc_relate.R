f2c5dcc65a7fd974735efa2d33003372fe807b4e
eboncourt <etienne.boncourt@gmail.com>
2026-01-06 14:24:37 +0100

    do data

---
+++ R/doc_relate.R
@@ -0,0 +1,12 @@
+#' relate
+#'
+#' Description.
+#'
+#' @format A data frame with 81 rows and 3 variables:
+#' \describe{
+#'   \item{ ind1 }{  factor }
+#'   \item{ ind2 }{  factor }
+#'   \item{ wang }{  numeric }
+#' }
+#' @source Source
+"relate"
---
+++ R/doc_samples.R
@@ -0,0 +1,12 @@
+#' samples
+#'
+#' Description.
+#'
+#' @format A data frame with 49 rows and 3 variables:
+#' \describe{
+#'   \item{ Individual }{  character }
+#'   \item{ geometry }{  sfc_POINT,sfc }
+#'   \item{ Sex }{  character }
+#' }
+#' @source Source
+"samples"
