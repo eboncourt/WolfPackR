4a3ce002d7e9ee7253da495324e19cd7ea87e141
eboncourt <etienne.boncourt@gmail.com>
2026-01-06 14:27:47 +0100

    Revise documentation for 'relate' function
    Updated documentation for the 'relate' function to include details about simulated pairwise relatedness estimators.

--- R/doc_relate.R
+++ R/doc_relate.R
@@ -1,7 +1,7 @@
 #' relate
 #'
 #' Description.
-#'
+#' A data frame containing simulated pairwise relatedness estimators (in this case, Wang's estimator) for 9 individuals.
 #' @format A data frame with 81 rows and 3 variables:
 #' \describe{
 #'   \item{ ind1 }{  factor }
@@ -10,3 +10,4 @@
 #' }
 #' @source Source
 "relate"
+
