259476cd9b80ef2bdaf8b5b8959c3a845c2f8568
eboncourt <etienne.boncourt@gmail.com>
2026-01-13 15:25:46 +0100

    Refactor relatedness matrix filtering and formatting

--- R/genetic_groups.R
+++ R/genetic_groups.R
@@ -21,7 +21,9 @@ genetic_groups <- function(relate, estimator, threshold = 0.4, samples) {
   indiv_list <- as.data.frame(unique(c(relate$ind1, relate$ind2)))
 
   # Filter and format the relatedness matrix
-  relate <- relate[, c("ind1", "ind2", estimator)] %>%
+  relate <- relate %>%
+    mutate(across(c(ind1, ind2), as.character)) %>%  # Convert to character to avoid problems with factors in dplyr
+    select(ind1, ind2, {{estimator}}) %>%
     filter(.data[[estimator]] > threshold)
 
   # Add diagonals
@@ -29,6 +31,7 @@ genetic_groups <- function(relate, estimator, threshold = 0.4, samples) {
   lvl2 <- levels(as.factor(relate$ind2))
   ind <- unique(c(lvl1, lvl2))
   tab <- data.frame(ind1 = ind, ind2 = ind, wang = NA)
+  names(tab)[3] <- estimator
   relate <- rbind(relate, tab)
 
   # Create the similarity matrix
