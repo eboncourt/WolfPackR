a8b1c4ba3be10ad09c8e6e280fe1a01875ee6894
eboncourt <etienne.boncourt@gmail.com>
2026-03-12 17:03:45 +0100

    Add documentation for Bruvo_Carpathians dataset
    Added documentation for the Bruvo_Carpathians dataset, including its description, format, and source.

---
+++ R/doc_Bruvo_Carpathians.R
@@ -0,0 +1,12 @@
+#' Bruvo_Carpathians
+#'
+#' Description.
+#' A data frame containing pairwise relatedness estimators (1 - Bruvo distance) for 177 individuals. This dataset is derived from the data taken from Iosif, R., Skrbinšek, T., Erős, N., Konec, M., Boljte, B., Jan, M., & Promberger‐Fürpass, B. (2025). Wolf Population Size and Composition in One of Europe’s Strongholds, the Romanian Carpathians. Ecology and Evolution, 15(4), e71200. https://doi.org/10.1002/ece3.71200
+#' @format A data frame with 6320 rows and 3 variables:
+#' \describe{
+#'   \item{ ind1 }{  character }
+#'   \item{ ind2 }{  character }
+#'   \item{ 1-Bruvo }{  numeric }
+#' }
+#' @source Source
+"Bruvo_Carpathians"
