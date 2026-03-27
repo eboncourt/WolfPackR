bf153c4ae27e82be15fe807ce52526e36d98cc5e
eboncourt <etienne.boncourt@gmail.com>
2026-03-27 13:18:47 +0100

    Create initial documentation for WolfPackR package
    Add package documentation for WolfPackR

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+#' @docType package
+#' @name WolfPackR
+#' @title WolfPackR: Analyzing Wolf Pack Territories
+#' @description A comprehensive toolkit for analyzing genetic and spatial structures of wolf packs.
+#' @importFrom knitr vignette_engine
+NULL
